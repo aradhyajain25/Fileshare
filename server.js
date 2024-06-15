e5bc7fc2a0ba43002671d8af3f9cad6fa6f314b2
anushkacodez <118354562+anushkacodez@users.noreply.github.com>
2024-06-15 19:52:17 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app=express();
+const path=require('path');
+const cors=require('cors');
+
+const PORT= process.env.PORT || 3000;
+app.use(express.static('public'));
+app.use(express.json());
+const connectDB = require('./config/db');
+connectDB();
+
+//Cors
+app.use(cors());
+
+//Template engine
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine','ejs');
+
+app.get('/', (req, res)=>{
+    res.render('index');
+})
+
+//Routes
+app.use('/api/files', require('./routes/files'));   
+app.use('/files', require('./routes/show'));  
+app.use('/files/download', require('./routes/download'));
+
+app.listen(PORT, ()=>{
+    console.log(`Listening on port ${PORT}`);
+})
